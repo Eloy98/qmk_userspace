083d1e23f2dc3b798e5000ea7d78f6fd79adbfbe
Eloy <52245532+Eloy98@users.noreply.github.com>
2024-02-04 14:10:49 +0800

    Update rgb.c

--- keyboards/sporkus/le_chiffre_32/rgb.c
+++ keyboards/sporkus/le_chiffre_32/rgb.c
@@ -34,11 +34,26 @@ led_config_t g_led_config = {
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     for (uint8_t i = led_min; i < led_max; i++) {
         switch(get_highest_layer(layer_state|default_layer_state)) {
+            case 1:
+                rgb_matrix_set_color(i, RGB_RED);
+                break;
             case 2:
+                rgb_matrix_set_color(i, RGB_ORANGE);
+                break;
+            case 3:
+                rgb_matrix_set_color(i, RGB_YELLOW);
+                break;
+            case 4:
+                rgb_matrix_set_color(i, RGB_GREEN);
+                break;
+            case 5:
+                rgb_matrix_set_color(i, RGB_CYAN);
+                break;
+            case 6:
                 rgb_matrix_set_color(i, RGB_BLUE);
                 break;
-            case 1:
-                rgb_matrix_set_color(i, RGB_YELLOW);
+            case 7:
+                rgb_matrix_set_color(i, RGB_PURPLE);d
                 break;
             default:
                 break;
