5e5f9e7c09de0ff0518d58dca51136265a67c245
Eloy <52245532+Eloy98@users.noreply.github.com>
2024-04-01 22:39:26 +0800

    change

--- keyboards/sporkus/le_chiffre_32/rgb.c
+++ keyboards/sporkus/le_chiffre_32/rgb.c
@@ -31,36 +31,53 @@ led_config_t g_led_config = {
     //  https://docs.qmk.fm/#/feature_rgb_matrix?id=flags
     { 8, 8, 2, 2, 2, 2, 2, 2, 2, 2, 2 }
 };
-bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    for (uint8_t i = led_min; i < led_max; i++) {
-        switch(get_highest_layer(layer_state|default_layer_state)) {
-            case 1:
-                rgb_matrix_set_color(i, RGB_RED);
-                break;
-            case 2:
-                rgb_matrix_set_color(i, RGB_ORANGE);
-                break;
-            case 3:
-                rgb_matrix_set_color(i, RGB_YELLOW);
-                break;
-            case 4:
-                rgb_matrix_set_color(i, RGB_GREEN);
-                break;
-            case 5:
-                rgb_matrix_set_color(i, RGB_CYAN);
-                break;
-            case 6:
-                rgb_matrix_set_color(i, RGB_BLUE);
-                break;
-            case 7:
-                rgb_matrix_set_color(i, RGB_PURPLE);
-                break;
-            default:
-                break;
-        }
-    }
-    return false;
+
+// Layer and Mods indicator
+#define LED_CENTER_TOP 1
+#define LED_CENTER_BOTTOM 0
+
+#define LAYER_R layer_colors[layer][0] *  RGB_INDICATOR_BRIGHTNESS / 255
+#define LAYER_G layer_colors[layer][1] *  RGB_INDICATOR_BRIGHTNESS / 255
+#define LAYER_B layer_colors[layer][2] *  RGB_INDICATOR_BRIGHTNESS / 255
+
+#define MODS_ACTIVE(mods) \
+    ((get_mods()|get_oneshot_mods()) & MOD_MASK_##mods ? RGB_INDICATOR_BRIGHTNESS:0)
+#define SHIFT_ACTIVE (get_mods() & MOD_MASK_SHIFT ? RGB_INDICATOR_BRIGHTNESS/4:0)
+#define MODS_R MODS_ACTIVE(CTRL) + SHIFT_ACTIVE
+#define MODS_G MODS_ACTIVE(GUI) + SHIFT_ACTIVE
+#define MODS_B MODS_ACTIVE(ALT) + SHIFT_ACTIVE
+
+const uint8_t PROGMEM layer_colors[][3] = {
+    {RGB_OFF},
+    {RGB_RED},
+    {RGB_GREEN},
+    {RGB_BLUE},
+    {RGB_YELLOW},
+    {RGB_PURPLE},
+    {RGB_PINK},
+    {RGB_TEAL}
+};
+
+void set_rgb_matrix_indicators(uint8_t led_min, uint8_t led_max) {
+    #if defined(RGB_LAYER_INDICATOR_ENABLE)
+    int layer = get_highest_layer(layer_state|default_layer_state);
+    RGB_MATRIX_INDICATOR_SET_COLOR(LED_CENTER_TOP, LAYER_R, LAYER_G, LAYER_B);
+    /* uprintf("layer RGB: (%u, %u, %u)\n", LAYER_R, LAYER_G, LAYER_B); */
+    #else
+    RGB_MATRIX_INDICATOR_SET_COLOR(LED_CENTER_TOP, 0, 0, 0);
+    #endif
+
+    #if defined(RGB_MODS_INDICATOR_ENABLE)
+    RGB_MATRIX_INDICATOR_SET_COLOR(LED_CENTER_BOTTOM, MODS_R, MODS_G, MODS_B);
+    /* uprintf("mod RGB: (%u, %u, %u)\n", MODS_R, MODS_G, MODS_B); */
+    #else
+    RGB_MATRIX_INDICATOR_SET_COLOR(LED_CENTER_BOTTOM, 0, 0, 0);
+    #endif
 }
 
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    set_rgb_matrix_indicators(led_min, led_max);
+    return false;
+}
 
 #endif
