f0fb7c06cd9c4c99ba80532db278cc055f452646
Eloy98 <1172240653@qq.com>
2024-05-13 14:42:52 +0800

    add 40rmie

---
+++ keyboards/xiix/40rmie/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x23, 0xD1, 0x23, 0xA0, 0x8A, 0x3A, 0x0A, 0x03}
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+#define TAPPING_TERM 250
+
