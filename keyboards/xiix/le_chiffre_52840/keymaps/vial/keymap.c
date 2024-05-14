4cd5546908affef59a8085d1f065105c2649582b
Eloy <52245532+Eloy98@users.noreply.github.com>
2024-05-14 23:08:49 +0800

    222

--- keyboards/xiix/le_chiffre_52840/keymaps/vial/keymap.c
+++ keyboards/xiix/le_chiffre_52840/keymaps/vial/keymap.c
@@ -20,8 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,      KC_MUTE,     KC_Y,   KC_U,   KC_I,    KC_O,   KC_P,
          HM_A,   HM_S,   HM_D,   HM_F,   KC_G,                   KC_H,   HM_J,   HM_K,    HM_L,   HM_QUOT,
          HM_Z,   KC_X,   KC_C,   KC_V,   KC_B,                   KC_N,   KC_M,   KC_COMM, KC_DOT, HM_SLSH,
-           LT(1, KC_ESC),  KC_SPC, LT(1,KC_SPC),                 LT(2,KC_SPC),KC_SPC, LT(2, KC_BSPC)
-    )
+           LT(1, KC_ESC),  KC_SPC, LT(1,KC_SPC),                 LT(2,KC_SPC),KC_SPC, LT(2, KC_BSPC))
 };
 
 #ifdef ENCODER_MAP_ENABLE
