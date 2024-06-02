e5c369dd4db05557424d8c1ee4b299f47b0853dc
Eloy <52245532+Eloy98@users.noreply.github.com>
2024-06-02 22:44:42 +0800

    test

--- keyboards/xiix/le_chiffre_32/keymaps/vial/keymap.c
+++ keyboards/xiix/le_chiffre_32/keymaps/vial/keymap.c
@@ -49,14 +49,3 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 };
 #endif
 
-bool oled_task_user(void) {
-    // Host Keyboard Layer Status
-    oled_write_P(PSTR("Layer: "), false);
-    // Host Keyboard LED Status
-    led_t led_state = host_keyboard_led_state();
-    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
-    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
-    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
-    
-    return false;
-}
