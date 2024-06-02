818934fc7e0ea3e87b4d5031c3c99e644db94ddf
Eloy <52245532+Eloy98@users.noreply.github.com>
2024-06-02 21:53:28 +0800

    111

---
+++ keyboards/xiix/printedpad/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xC9, 0xD4, 0x38, 0x79, 0x25, 0x61, 0x99, 0x88}
+#define VIAL_COMBO_ENTRIES 64
+#define TAPPING_TERM 250
