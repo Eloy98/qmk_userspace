26cc3fa2e6746b75fc4fa0fa03593340c90a3836
Eloy <52245532+Eloy98@users.noreply.github.com>
2024-06-06 12:30:23 +0800

    caps

--- keyboards/xiix/qazikat/keymaps/vial/keymap.c
+++ keyboards/xiix/qazikat/keymaps/vial/keymap.c
@@ -32,43 +32,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 };
 
-// const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-//     {0, 3, HSV_RED}     
-// );
+#ifdef RGBLIGHT_ENABLE
 
-const rgblight_segment_t PROGMEM _base_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-        {0, 3, HSV_BLUE}
-        );
-const rgblight_segment_t PROGMEM _middle_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-        {0, 3, HSV_GREEN}
-        );
-const rgblight_segment_t PROGMEM _top_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-        {0, 3, HSV_RED}
-        );
+const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, 3, HSV_RED}
+);
 
-const rgblight_segment_t* const PROGMEM _rgb_layers[] =
-RGBLIGHT_LAYERS_LIST(
-        _base_layer,
-        _middle_layer,
-        _top_layer
-        );
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+    my_capslock_layer
+);
 
-void keyboard_post_init_user(void) {
-    rgblight_layers = _rgb_layers;
+bool led_update_kb(led_t led_state) {
+    if (!led_update_user(led_state)) { return false; }
+    rgblight_set_layer_state(0, led_state.caps_lock);
+    return true;
 }
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-
-    switch (get_highest_layer(state)) {
-        case 0:
-            rgblight_blink_layer(0, 500);
-            break;
-        case 1:
-            rgblight_blink_layer(1, 500);
-            break;
-        case 2:
-            rgblight_blink_layer(2, 500);
-            break;
-    }
-    return state;
+void keyboard_post_init_kb(void) {
+    rgblight_layers = my_rgb_layers;
+    keyboard_post_init_user();
 }
