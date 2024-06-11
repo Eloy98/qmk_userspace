0c8e8b0c992408d9da8061549c9b1995d14955d7
Eloy98 <eloy1172240653@gmail.com>
2024-06-11 22:47:32 +0800

    add cheerz36

---
+++ keyboards/xiix/cheerz36/keymaps/vial/keymap.c
@@ -0,0 +1,13 @@
+// Copyright 2023 sporkus
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+#include "../../rgb.c"
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+     [0] = LAYOUT(
+        KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   
+        KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, 
+        KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  
+        KC_NO,  KC_NO,          KC_NO,  KC_NO,  KC_NO,  KC_NO,          KC_NO,  KC_NO)
+};
---
+++ keyboards/xiix/cheerz36/rgb.c
@@ -0,0 +1,36 @@
+#ifdef RGB_MATRIX_ENABLE
+// LED index
+// 9    10    2    3
+//         1
+//         0
+//   8 7   6   5 4
+
+led_config_t g_led_config = {
+    // Key Matrix to LED Index
+    {
+         {9,     9,    9,   9,    9,            3, 3, 3,    3,     3 },
+         {10,    10,   10,  10,   10,           2, 2, 2,    2,     2 },
+         {8,     8,    8,   8,    8,            4, 4, 4,    4,     4 },
+         {NO_LED, NO_LED, 7, NO_LED,      6,    6, 6, 5, NO_LED, NO_LED, }
+    },
+    // LED Index to Physical Positon
+    {
+        { 128, 40 },
+        { 128, 30 },
+        { 166, 0 },
+        { 255, 0 },
+        { 255, 48 },
+        { 178, 64 },
+        { 128, 64 },
+        { 77, 64 },
+        { 0, 48 },
+        { 0, 0 },
+        { 115, 0 }
+    },
+    //  LED Index to Flag
+    //  https://docs.qmk.fm/#/feature_rgb_matrix?id=flags
+    { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 }
+};
+
+
+#endif
